186d2c87480f159fc493b8aff29fc27fd3222eaf
arif mahmud <mahmudarif1175@gmail.com>
2023-12-06 17:10:28 +0600

    intendatian error create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="main">
-      <div>
+    <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
