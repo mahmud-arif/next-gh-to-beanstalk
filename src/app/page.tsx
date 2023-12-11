55bca7c668cb8fd2bfb5267869bf78792cf7e597
arif mahmud <mahmudarif1175@gmail.com>
2023-12-11 13:05:53 +0600

    test ch-set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="main">
       <div>
-        <h2>Hello World kldjfkd</h2>
-        <h3>hi</h3>
+        <h2>Hello World</h2>
+        <h3>hi dkjfkd</h3>
       </div>
       {/* <Link href="/about">About</Link> */}
     </main>
